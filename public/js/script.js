f758662bf29f8468419f632950253714c51c62d3
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-07-07 14:59:06 -0300

    feat: toggleDrum function
    This function flips the boolean value in the correct array at the specified index.

--- public/js/script.js
+++ public/js/script.js
@@ -4,3 +4,31 @@ let snares = new Array(16).fill(false);
 let hiHats = new Array(16).fill(false);
 let rideCymbals = new Array(16).fill(false);
 
+const toggleDrum = (drumType, index) => {
+  if (index < 0 || index >= 16) {
+    return;
+  }
+
+  if (typeof drumType !== 'string') {
+    return;
+  }
+
+  if (!['kicks', 'snares', 'hiHats', 'rideCymbals'].includes(drumType)) {
+    return;
+  }
+
+  switch (drumType) {
+    case 'kicks':
+      kicks[index] = !kicks[index];
+      break;
+    case 'snares':
+      snares[index] = !snares[index];
+      break;
+    case 'hiHats':
+      hiHats[index] = !hiHats[index];
+      break;
+    case 'rideCymbals':
+      rideCymbals[index] = !rideCymbals[index];
+      break;
+  }
+}
