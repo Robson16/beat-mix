709d96b1632e03d49e6f2d667cb79f187d265bd1
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-07-07 16:44:52 -0300

    feat: server-side function presetHandler
    The function saves and retrieves drum machine presets

--- presetHandler.js
+++ presetHandler.js
@@ -1,8 +1,22 @@
-// Use this presets array inside your presetHandler
 const presets = require('./presets');
 
-// Complete this function:
-const presetHandler = () => {};
+const presetHandler = (method, index, newPresetArray) => {
+  if (method !== 'GET' && method !== 'PUT') {
+    return [400];
+  }
 
-// Leave this line so that your presetHandler function can be used elsewhere:
+  if (index < 0 || index >= presets.length || !Number.isInteger(index)) {
+    return [404];
+  }
+
+  if (method === 'GET') {
+    return [200, presets[index]];
+  }
+
+  if (method === 'PUT') {
+    presets[index] = newPresetArray;
+    return [200, presets[index]];
+  }
+}
+
 module.exports = presetHandler;
